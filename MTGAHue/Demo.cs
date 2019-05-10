4e44d5cb7de7e94f647980af203c08d7b512d4a9
Jon Radchenko <jonrad@users.noreply.github.com>
2019-05-10 17:21:48 -0400

    Add ability to run in demo mode (#3)

---
+++ MTGAHue/Demo.cs
@@ -0,0 +1,60 @@
+﻿using MTGADispatcher;
+using MTGADispatcher.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MTGAHue
+{
+    public class Demo
+    {
+        private static Dictionary<char, MagicColor> colorMap = new Dictionary<char, MagicColor>
+        {
+            ['R'] = MagicColor.Red,
+            ['G'] = MagicColor.Green,
+            ['U'] = MagicColor.Blue,
+            ['B'] = MagicColor.Blue,
+            ['W'] = MagicColor.White
+        };
+
+        private readonly Game game;
+
+        private int instanceId = 0;
+
+        public Demo(Game game)
+        {
+            this.game = game;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Q quits. Multiple colors ok (Eg: RG)");
+            while (true)
+            {
+                Console.WriteLine("Select color [R]ed/[G]reen/bl[U]e/[B]lack/[W]hite]:");
+                var input = Console.ReadLine()?.ToUpperInvariant();
+                if (input == null || input == "Q")
+                {
+                    return;
+                }
+
+                var colors = ParseColors(input).ToArray();
+                instanceId++;
+
+                var instance = new Instance(instanceId, instanceId, colors);
+                game.Events.Dispatch(new CastSpell(instance));
+            }
+        }
+
+        private IEnumerable<MagicColor> ParseColors(string input)
+        {
+            foreach (var c in input)
+            {
+                if (colorMap.TryGetValue(c, out var color))
+                {
+                    yield return color;
+                }
+            }
+        }
+    }
+}
