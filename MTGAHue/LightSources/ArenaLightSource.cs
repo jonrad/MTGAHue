82c751951abd198d03d29d9a03ba4df05984bbd8
jonrad <jonrad@gmail.com>
2019-05-10 17:03:50 -0400

    Moved to proper location

--- MTGAHue/LightSources/ArenaLightSource.cs
+++ MTGAHue/LightSources/ArenaLightSource.cs
@@ -1,14 +1,11 @@
-﻿using LightsApi.LightSources;
+﻿using LightsApi;
+using LightsApi.LightSources;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Windows.Forms;
 
-namespace LightsApi.WinForms
+namespace MTGAHue.LightSources
 {
-    public partial class Demo : Form
-    {
     // Imagine the arena broken up into a grid, where X = -1 is the far left, X = 1 the far right
     // Y = 1 is the furthest in front (Where the opponent is), Y = -1 is the very back (Where the player is)
     // We can think of an Arena as two halves: the opponent and the player
@@ -76,45 +73,6 @@ namespace LightsApi.WinForms
 
                 var rgb = lightSource.Calculate(x, lineY);
                 return rgb * multiplier;
-            }
-        }
-    }
-
-        public Demo()
-        {
-            InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            var lightSource = new ArenaLightSource(
-                new[] { RGB.Green },
-                new[] { RGB.Blue, RGB.Red });
-
-            var count = 100;
-
-            using (var graphics = pictureBox1.CreateGraphics())
-            {
-                // Transforms our coordinate system to make it -1 <= X <= 1, -1 <= Y <= 1
-                // AKA a 2x2 grid starting at top left -1, -1 and ending at bottom right 1, 1
-                graphics.ScaleTransform(pictureBox1.Width / 2f, pictureBox1.Height / 2f);
-                graphics.TranslateTransform(1, 1);
-
-                var stepSize = 2f / count;
-                for (var i = 0; i < count; i++)
-                {
-                    for (var j = 0; j < count; j++)
-                    {
-                        var x = -1f + i * stepSize;
-                        var y = -1f + j * stepSize;
-
-                        var color = lightSource.Calculate(x + stepSize / 2, y + stepSize / 2);
-
-                        graphics.FillRectangle(
-                            new SolidBrush(Color.FromArgb(color.R, color.G, color.B)),
-                            x, y, 2f / count, 2f / count);
-                    }
-                }
             }
         }
     }
