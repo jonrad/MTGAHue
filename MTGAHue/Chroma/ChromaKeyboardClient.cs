ae5719696f3d34f681c83b4987d6788e90ef0a08
jonrad <jonrad@gmail.com>
2019-05-11 15:04:37 -0400

    Revert "Revert "[Feature] Work with Chroma (Razer) keyboards""
    This reverts commit c118739acb4c57b8d950c385272cbd68b3ada797.

---
+++ MTGAHue/Chroma/ChromaKeyboardClient.cs
@@ -0,0 +1,53 @@
+﻿using Colore;
+using Colore.Effects.Keyboard;
+using LightsApi;
+using LightsApi.Transitions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MTGAHue.Chroma
+{
+    public class KeyboardLayout : ILightLayout
+    {
+        private readonly IKeyboard keyboard;
+
+        public KeyboardLayout(IKeyboard keyboard)
+        {
+            this.keyboard = keyboard;
+        }
+
+        public Task Transition(ITransition transition, CancellationToken token)
+        {
+            return Task.FromResult(0):
+            /*keyboard.SetCustomAsync(
+                new KeyboardCustom)*/
+        }
+
+        private class KeyboardPosition
+        {
+            public KeyboardPosition(int column, int row, double x, double y)
+            {
+            }
+        }
+    }
+
+    public class ChromaKeyboardClient : ILightClient
+    {
+        private IChroma chroma;
+
+        public ILightLayout GetLayout()
+        {
+            return new KeyboardLayout(chroma.Keyboard);
+        }
+
+        public async Task Start(CancellationToken token)
+        {
+            chroma = await ColoreProvider.CreateNativeAsync();
+        }
+
+        public Task Stop(CancellationToken token)
+        {
+            return Task.FromResult(0):
+        }
+    }
+}
