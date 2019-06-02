d13d66f6f54e46b1c48815ac125b476512d2087b
jonrad <jonrad@gmail.com>
2019-06-02 16:12:41 -0400

    Cleanuped up cue

--- LightsApi.Cue/CueLightClient.cs
+++ LightsApi.Cue/CueLightClient.cs
@@ -1,57 +1,23 @@
-﻿using CUE.NET;
-using CUE.NET.Brushes;
-using CUE.NET.Devices.Generic;
-using CUE.NET.Devices.Generic.Enums;
+﻿using CUE.NET.Devices.Generic;
 using CUE.NET.Devices.Keyboard;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace LightsApi.Cue
 {
-    //TODO refactor this whole thing
     public class CueLightClient : ILightClient
     {
-        private KeyboardPosition[] keyboardPositions;
+        private LedPosition[] ledPositions;
 
         private CorsairKeyboard keyboard;
 
-        public CueLightClient()
+        internal CueLightClient(CorsairKeyboard keyboard, LedPosition[] ledPositions)
         {
-            CueSDK.Initialize(true);
-            keyboard = CueSDK.KeyboardSDK;
-            keyboard.Brush = (SolidColorBrush)Color.Transparent;
-
-            float left = float.MaxValue, top = float.MaxValue;
-            float right = float.MinValue, bottom = float.MinValue;
-
-            left = keyboard[CorsairLedId.Escape].LedRectangle.Left;
-            top = keyboard[CorsairLedId.Escape].LedRectangle.Top;
-            bottom = keyboard[CorsairLedId.Space].LedRectangle.Bottom;
-            right = keyboard[CorsairLedId.F12].LedRectangle.Right;
-
-            var width = right - left;
-            var height = bottom - top;
-
-            keyboardPositions = keyboard.Select(key =>
-            {
-                var rect = key.LedRectangle;
-                var x = -1 + (rect.Left + (rect.Right - rect.Left) / 2f - left) / (width / 2f);
-                var y = 1 - (rect.Top + (rect.Bottom - rect.Top) / 2 - top) / (height / 2f);
-
-                if (x < -1 || x > 1 || y < -1 || y > 1)
-                {
-                    return null;
-                }
-
-                return new KeyboardPosition(key, new Position(x, y));
-            })
-            .Where(k => k != null)
-            .ToArray();
-
-            Lights = keyboardPositions.Select(k => k.Position).ToArray();
+            this.keyboard = keyboard;
+            this.ledPositions = ledPositions;
+            Lights = ledPositions.Select(k => k.Position).ToArray();
         }
 
         public IEnumerable<Position> Lights { get; }
@@ -63,7 +29,7 @@ namespace LightsApi.Cue
                 var i = 0;
                 foreach (var color in colors)
                 {
-                    keyboardPositions[i].Led.Color = new CorsairColor(
+                    ledPositions[i].Led.Color = new CorsairColor(
                         (byte)color.R, (byte)color.G, (byte)color.B);
 
                     i++;
@@ -71,19 +37,6 @@ namespace LightsApi.Cue
 
                 keyboard.Update();
             }, token);
-        }
-
-        private class KeyboardPosition
-        {
-            public KeyboardPosition(CorsairLed led, Position position)
-            {
-                Led = led;
-                Position = position;
-            }
-
-            public CorsairLed Led { get; }
-
-            public Position Position { get; }
         }
     }
 }
--- MagicLights/LightClients/CueLightClientProvider.cs
+++ MagicLights/LightClients/CueLightClientProvider.cs
@@ -1,24 +1,40 @@
-﻿using LightsApi;
+﻿using CUE.NET.Devices.Generic.Enums;
+using LightsApi;
 using LightsApi.Cue;
-using System;
 using System.Threading.Tasks;
 
 namespace MagicLights.LightClients
 {
     public class CueLightClientProvider : AbstractLightClientProvider<CueLightClientProvider.CueConfiguration>
     {
-        private readonly Lazy<CueLightClient> chromaTask =
-            new Lazy<CueLightClient>(() => new CueLightClient());
+        private readonly CueLightClientBuilder builder;
 
         public override string Id { get; } = "cue";
 
+        public CueLightClientProvider()
+        {
+            builder = new CueLightClientBuilder();
+        }
+
         public override Task<ILightClient> CreateAsync(CueConfiguration configuration)
         {
-            return Task.FromResult<ILightClient>(chromaTask.Value);
+            return Task.FromResult<ILightClient>(
+                builder.Build(
+                    configuration.LeftLed,
+                    configuration.RightLed,
+                    configuration.TopLed,
+                    configuration.BottomLed));
         }
 
         public class CueConfiguration
         {
+            public CorsairLedId? LeftLed { get; set; }
+
+            public CorsairLedId? RightLed { get; set; }
+
+            public CorsairLedId? TopLed { get; set; }
+
+            public CorsairLedId? BottomLed { get; set; }
         }
     }
 }
