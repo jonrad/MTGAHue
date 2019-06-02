a9c78212a7972e82e315d931332a9396d85d997c
jonrad <jonrad@gmail.com>
2019-06-02 16:12:41 -0400

    Cleanuped up cue

---
+++ LightsApi.Cue/CueLightClientBuilder.cs
@@ -0,0 +1,63 @@
+﻿using CUE.NET;
+using CUE.NET.Brushes;
+using CUE.NET.Devices.Generic.Enums;
+using System.Drawing;
+using System.Linq;
+
+namespace LightsApi.Cue
+{
+    public class CueLightClientBuilder
+    {
+        public CueLightClient Build(
+            CorsairLedId? leftLed,
+            CorsairLedId? rightLed,
+            CorsairLedId? topLed,
+            CorsairLedId? bottomLed)
+        {
+            CueSDK.Initialize(true);
+            var keyboard = CueSDK.KeyboardSDK;
+            keyboard.Brush = (SolidColorBrush)Color.Transparent;
+
+            float left = float.MaxValue, top = float.MaxValue;
+            float right = float.MinValue, bottom = float.MinValue;
+
+            left = leftLed == null 
+                ? keyboard.Min(l => l.LedRectangle.Left) 
+                : keyboard[leftLed.Value].LedRectangle.Left;
+
+            right = rightLed == null
+                ? keyboard.Max(l => l.LedRectangle.Right)
+                : keyboard[rightLed.Value].LedRectangle.Right;
+
+            top = topLed == null
+                ? keyboard.Min(l => l.LedRectangle.Top)
+                : keyboard[topLed.Value].LedRectangle.Top;
+
+            bottom = bottomLed == null
+                ? keyboard.Max(l => l.LedRectangle.Bottom)
+                : keyboard[bottomLed.Value].LedRectangle.Bottom;
+
+            var width = right - left;
+            var height = bottom - top;
+
+            var ledPositions = keyboard.Select(key =>
+            {
+                var rect = key.LedRectangle;
+                var x = -1 + (rect.Left + (rect.Right - rect.Left) / 2f - left) / (width / 2f);
+                var y = 1 - (rect.Top + (rect.Bottom - rect.Top) / 2 - top) / (height / 2f);
+
+                if (x < -1 || x > 1 || y < -1 || y > 1)
+                {
+                    return null;
+                }
+
+                return new LedPosition(key, new Position(x, y));
+            })
+            .Where(k => k != null)
+            .Cast<LedPosition>()
+            .ToArray();
+
+            return new CueLightClient(keyboard, ledPositions);
+        }
+    }
+}
---
+++ LightsApi.Cue/LedPosition.cs
@@ -0,0 +1,17 @@
+﻿using CUE.NET.Devices.Generic;
+
+namespace LightsApi.Cue
+{
+    public class LedPosition
+    {
+        public LedPosition(CorsairLed led, Position position)
+        {
+            Led = led;
+            Position = position;
+        }
+
+        public CorsairLed Led { get; }
+
+        public Position Position { get; }
+    }
+}
