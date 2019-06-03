f7afc0994be3b054779ac431db342b003d966a85
jonrad <jonrad@gmail.com>
2019-06-02 20:44:38 -0400

    [Refactor] Make Transitions Great Again

--- LightsApi/Transitions/CompositeTransition.cs
+++ LightsApi/Transitions/CompositeTransition.cs
@@ -1,27 +1,66 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
 
 namespace LightsApi.Transitions
 {
     public class CompositeTransition : ITransition
     {
-        private readonly ITransition[] transitions;
+        private readonly TimedTransition[] transitions;
+
+        private int currentIndex = 0;
 
         public CompositeTransition(params ITransition[] transitions)
         {
-            this.transitions = transitions;
+            var startTime = 0l;
+            this.transitions = transitions.Select(t =>
+            {
+                var endTime = startTime + (long)t.TotalLength.TotalMilliseconds;
+                var result = new TimedTransition(startTime, endTime, t);
+                startTime = endTime;
+
+                return result;
+            }).ToArray();
+
+            TotalLength = TimeSpan.FromMilliseconds(transitions.Sum(t => t.TotalLength.TotalMilliseconds));
         }
 
-        public async Task Transition(ILayer layer, CancellationToken token = default)
+        public TimeSpan TotalLength { get; }
+
+        public RGB Get(float x, float y, long ms)
         {
-            foreach (var transition in transitions)
+            var transition = transitions[currentIndex];
+
+            if (transition.StartTime > ms)
             {
-                if (token.IsCancellationRequested)
+                currentIndex = 0;
+            }
+
+            while ((transition = transitions[currentIndex]).EndTime < ms)
+            {
+                if (currentIndex == transitions.Length - 1)
                 {
-                    return;
+                    break;
                 }
 
-                await transition.Transition(layer, token);
+                currentIndex++;
+            }
+
+            return transition.Transition.Get(x, y, ms - transition.StartTime);
+        }
+
+        public class TimedTransition
+        {
+            public long StartTime { get; }
+
+            public long EndTime { get; }
+
+            public ITransition Transition { get; }
+
+            public TimedTransition(long startTime, long endTime, ITransition transition)
+            {
+                StartTime = startTime;
+                EndTime = endTime;
+                Transition = transition;
             }
         }
     }
